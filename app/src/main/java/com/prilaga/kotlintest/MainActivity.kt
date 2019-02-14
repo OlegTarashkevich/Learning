bf67383caa832fb82fd928e53ce6e30a234f1a6f
Oleg Tarashkevich <oleg.tarashkevich@mobexs.com>
2019-02-14 17:30:45 +0300

    first commit

---
+++ app/src/main/java/com/prilaga/kotlintest/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.prilaga.kotlintest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
+import java.util.concurrent.atomic.AtomicLong
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        push_button.setOnClickListener { doIt() }
+    }
+
+    fun doIt() {
+
+        println("Start")
+
+        // Start a coroutine
+        GlobalScope.launch {
+            delay(1000)
+            println("Hello")
+        }
+
+        Thread.sleep(2000) // wait for 2 seconds
+        println("Stop")
+
+    }
+
+    fun fasterThanTreads() {
+
+        val value = AtomicLong()
+        for (i in 1..1_000_000L) {
+            GlobalScope.launch { value.getAndAdd(i) }
+        }
+
+        println(value.get())
+    }
+}
