93fba3aecb8056c77aeb2616a2049d15a0843b62
Oleg Tarashkevich <oleg.tarashkevich@mobexs.com>
2019-03-28 17:32:05 +0300

    Create a first network request with coroutines

--- app/src/main/java/com/prilaga/news/ui/MainActivity.kt
+++ app/src/main/java/com/prilaga/news/ui/MainActivity.kt
@@ -3,7 +3,9 @@ package com.prilaga.news.ui
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import com.prilaga.news.R
+import com.prilaga.news.data.network.model.Source
 import com.prilaga.news.viewmodel.SourceViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -20,6 +22,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         test_button.setOnClickListener { sourceViewModel.loadNews() }
+
+        sourceViewModel.sourceData.observe(this, Observer<Source> {
+
+        })
     }
 
 }
--- app/src/main/java/com/prilaga/news/viewmodel/SourceViewModel.kt
+++ app/src/main/java/com/prilaga/news/viewmodel/SourceViewModel.kt
@@ -3,17 +3,39 @@ package com.prilaga.news.viewmodel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.prilaga.news.data.NewsRepository
+import com.prilaga.news.data.network.model.RequestParam
 import com.prilaga.news.data.network.model.Source
+import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers.IO
 
 /**
  * Created by Oleg Tarashkevich on 28/03/2019.
  */
 class SourceViewModel(val repository: NewsRepository) : ViewModel() {
 
-    val data = MutableLiveData<Source>()
+    val sourceData = MutableLiveData<Source>()
 
-    fun loadNews(){
+    val viewModelJob = Job()
+    val viewModelScope = CoroutineScope(Dispatchers.Main + viewModelJob)
 
+    fun loadNews() {
+        viewModelScope.launch {
+
+            val source = withContext (IO) {
+                val param = Source.param(
+                    RequestParam.Category.BUSINESS,
+                    RequestParam.Language.EN,
+                    RequestParam.Country.US
+                )
+                 repository.getSources(param).await()
+            }
+
+            sourceData.value = source
+        }
     }
-    
+
+    fun cancelJob() {
+        viewModelJob.cancel()
+    }
+
 }
